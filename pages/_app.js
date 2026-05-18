95baa4a3be8ad3434611582172ede59109556e8f
Errenris <138004039+Errenris@users.noreply.github.com>
2026-05-19 01:03:35 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* Google AdSense Verification */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3215724068998032"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-
       <Component {...pageProps} />
+
+      <Script
+        src="https://pl29487742.effectivecpmnetwork.com/90/10/fe/9010fe6ef6eb78b9469467dbbf2273a0.js"
+        strategy="afterInteractive"
+      />
     </>
   )
 }
