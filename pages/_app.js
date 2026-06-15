b08f761b1fdae0657e3b829003994aef6b71581e
Errenris <138004039+Errenris@users.noreply.github.com>
2026-06-16 02:49:48 +0700

    Add page transition animations using framer-motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import '../styles/globals.css'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{ duration: 0.4 }}
+        variants={{
+          initialState: { opacity: 0, y: 15 },
+          animateState: { opacity: 1, y: 0 },
+          exitState: { opacity: 0, y: -15 },
+        }}
+        className="min-h-screen"
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  )
 }
